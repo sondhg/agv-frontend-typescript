4435b9b4cbd4f4dd3db4d8d65bba1e7ae6120518
sondhg <sondhw@gmail.com>
2024-11-06 16:20:39 +0700

    refactor: update startPoints and endPoints to return numbers, modify loadNames to use lowercase values, and add loadNamesEnum for enum validation

--- src/utils/arraysUsedOften.ts
+++ src/utils/arraysUsedOften.ts
@@ -1,17 +1,16 @@
 //from 1 to 15
-export const startPoints = Array.from({ length: 4 }, (_, index) =>
-  (index + 1).toString(),
-);
+export const startPoints = Array.from({ length: 4 }, (_, index) => index + 1);
 
 //from 10 to 15
-export const endPoints = Array.from({ length: 6 }, (_, index) =>
-  (index + 10).toString(),
-);
+export const endPoints = Array.from({ length: 6 }, (_, index) => index + 10);
 
-export const loadNames = ["Stone", "Cement", "Iron"];
+export const loadNames = ["stone", "wood", "iron"];
 
 export const agvIDs = Array.from({ length: 4 }, (_, index) =>
   (index + 1).toString(),
 );
 
 export const guidanceTypes = ["Line Following", "Computer Vision"];
+
+// Ensure loadNames is treated as a tuple with at least one string for enum validation in forms
+export const loadNamesEnum = loadNames as [string, ...string[]];
